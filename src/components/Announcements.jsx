300f3f6e51075c2b0f35184c61162cdd37fd1f5c
Yash Darbar <darbaryash44@gmail.com>
2023-10-19 04:32:47 +0530

    new commits

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    height:30px;
+    background-color:teal;
+    color:white;
+    align-items:center;
+    display:flex;
+    justify-content:center;
+    font-size:14px;
+    font-weight:500;
+    `
 
 const Announcements = () => {
-    return <div>Announcements</div>;
+    return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
 };
 
 export default Announcements;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Announcements from "../components/Announcements";
 
 const Home = () => {
-    return <div><Navbar/></div>;
+    return (
+        <div>
+            <Announcements />
+            <Navbar />
+        </div>
+    );
 };
 
 export default Home;
